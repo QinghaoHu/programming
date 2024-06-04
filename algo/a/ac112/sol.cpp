4051768aedcd33c98e478bdf82cf97d5b9cbee48
QinghaoHu <350681565@gapps.yrdsb.ca>
2024-06-04 07:28:37 -0400

    Update

---
+++ algo/a/ac112/sol.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <cstdio>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <vector>
+#include <array>
+#include <list>
+#include <deque>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <cctype>
+#include <ctime>
+#include <climits>
+
+using namespace std;
+using ll = long long;
+
+void solve() {
+    int n, d;
+    cin >> n >> d;
+    vector<pair<int, int> > pos(n);
+    for (auto &i : pos) {
+        cin >> i.first >> i.second;
+    }
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie (nullptr);
+
+    int T = 1;
+    //cin >> T;
+
+    while(T --) {
+        solve();
+    }
+
+    return 0;
+}
+
+/* stuff you should look for
+ * int overflow, array bounds
+ * special cases (n=1?)
+ * do smth instead of nothing and stay organized
+ * WRITE STUFF DOWN
+ * DON'T GET STUCK ON ONE APPROACH
+ */
