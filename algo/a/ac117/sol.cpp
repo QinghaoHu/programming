1f25801d5c1523eb9a985e66c2a9c285f65fa7d9
Qinghao Hu Mac <350681565@gapps.yrdsb.ca>
2024-06-06 13:52:53 -0400

    For ac117

--- algo/a/ac117/sol.cpp
+++ algo/a/ac117/sol.cpp
@@ -27,11 +27,48 @@
 using namespace std;
 using ll = long long;
 
+int num(char s) {
+    string str1 = "A234567890JQK";
+    return str1.find(s) + 1;
+}
+
 void solve() {
     vector<char> card[14], back[14];
     for (int i = 1; i <= 13; i++) {
-
+        for (int j = 0; j < 4; j++) {
+            char s;
+            cin >> s;
+            back[i].push_back(s);
+        }
+        reverse(back[i].begin(), back[i].end());
     }
+    //From bottom to height
+    for (int i = 0; i < 4; i++) {
+        char c = back[13].back();
+        back[13].pop_back();
+        while(c != 'K') {
+            int pos = num(c);
+            card[pos].push_back(c);
+            if (back[pos].size() == 0) {
+                c = card[pos][0];
+                card[pos].erase(card[pos].begin());
+            } else {
+                c = back[pos][0];
+                back[pos].erase(back[pos].begin());
+            }
+        }
+    }
+    vector<int> cnt(14, 0);
+    for (auto i : card) {
+        for (auto j : i) {
+            cnt[num(j)]++;
+        }
+    }
+    int ans = 0;
+    for (int i : cnt) {
+        if (i >= 4) ans++;
+    }
+    cout << ans << endl;
 }
 
 int main() {
