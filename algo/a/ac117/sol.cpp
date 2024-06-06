e593c29bdbe817b832018ea7d57c5b40d24d0f59
Qinghao Hu Mac <350681565@gapps.yrdsb.ca>
2024-06-06 10:41:18 -0400

    update

---
+++ algo/a/ac117/sol.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <cstdio>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <vector>
+#include <array>
+#include <list>
+#include <deque>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <cctype>
+#include <ctime>
+#include <climits>
+
+using namespace std;
+using ll = long long;
+
+void solve() {
+    vector<char> card[14], back[14];
+    for (int i = 1; i <= 13; i++) {
+
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie (nullptr);
+
+    int T = 1;
+    //cin >> T;
+
+    while(T --) {
+        solve();
+    }
+
+    return 0;
+}
+
+/* stuff you should look for
+ * int overflow, array bounds
+ * special cases (n=1?)
+ * do smth instead of nothing and stay organized
+ * WRITE STUFF DOWN
+ * DON'T GET STUCK ON ONE APPROACH
+ */
