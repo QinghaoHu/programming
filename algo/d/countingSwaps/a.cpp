7ebdd3a7991795ff29140a799aa7e90f2e791edc
QinghaoHu <huqinghao@outlook.com>
2024-08-25 18:18:02 -0400

    update

--- algo/d/countingSwaps/a.cpp
+++ algo/d/countingSwaps/a.cpp
@@ -14,7 +14,7 @@ typedef long long ll;
 typedef double db;
 typedef vector<int> VI;
 typedef pair<int, int> PII;
-const int mod = 999971;
+const int mod = 1e9 + 7;
 const db eps = 1e-9;
 const db PI = acos(-1.0);
 const int INF = 0x3f3f3f3f;
@@ -25,11 +25,65 @@ ll lcm(ll a, ll b) {return a / gcd(a, b) * b;}
 #define debug(x) cerr << #x << " = " << x << '\n';
 #endif
 
+const int N = 1e5 + 10;
+ll jc[N];
+bool st[N];
+int p[N];
+
+ll qmi(ll a, int b) {
+    ll c = 1;
+    for (; b; b >>= 1) {
+        if (b & 1) {
+            c = a * c % mod;
+        }
+        a = a * a % mod;
+    }
+    return c;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    memset(p, 0, sizeof p);
+    memset(st, 0, sizeof st);
+    for (int i = 1; i <= n; i++) {
+        cin >> p[i];
+    }
+    int cnt = 0;
+    ll res = 1;
+    for (int i = 1; i <= n; i++) {
+        if (st[i]) {
+            continue;
+        }
+        cnt++;
+        int l = 1;
+        st[i] = 1;
+        for (int j = p[i]; j != i; j = p[j]) {
+            l++;
+            st[j] = 1;
+        }
+        if (l > 1) {
+            res = res * qmi(l, l - 2) % mod;
+        }
+        res = res * qmi(jc[l - 1], mod - 2) % mod;
+    }
+    res = res * jc[n - cnt] % mod;
+    cout << res << "\n";
+}
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
-
+    jc[0] = 1;
+    for (int i = 1; i < N; i++) {
+        jc[i] = jc[i - 1] * i % mod;
+    }
+    int tt;
+    cin >> tt;
+    while(tt--) {
+        solve();
+    }
 
     return 0;
 }
