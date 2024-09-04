abd3c029be7a0cda4ff2a3ed83e1f4e755c01eea
QinghaoHu <huqinghao@outlook.com>
2024-09-04 12:01:26 -0400

    up

---
+++ algo/d/ac216/a.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define per(i, a, n) for (int i = a; i >= n; i--)
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+#define arr array
+#define SZ(x) ((int)x.size())
+typedef long long ll;
+typedef double db;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+const db eps = 1e-9;
+const db PI = acos(-1.0);
+const int INF = 0x3f3f3f3f;
+ll gcd(ll a, ll b) {return !b ? a : gcd(b, a % b);}
+ll powmod(ll a, ll b, ll p) { ll res=1; for(;b;b>>=1){ if(b&1){ res=res*a%p;} a=a*a%p;} return res;}
+ll lcm(ll a, ll b) {return a / gcd(a, b) * b;}
+
+const int N = 1e5 + 10;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+
+
+    return 0;
+}
