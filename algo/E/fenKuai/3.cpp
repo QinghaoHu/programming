046782fcfb672dd000d0bfb4d52618be795ae4ee
QinghaoHu <huqinghao@outlook.com>
2024-09-24 07:49:29 -0400

    update

--- algo/E/fenKuai/3.cpp
+++ algo/E/fenKuai/3.cpp
@@ -31,42 +31,47 @@ ll gcd(ll a, ll b) {return !b ? a : gcd(b, a%b);}
 ll powmod(ll a, ll b, ll p) {ll res=1;for(;b;b>>=1){if(b&1){res=res*a%p;}a=a*a%p;}return res;}
 ll lcm(ll a, ll b) {return a /gcd(a,b)*b;}
 
-const int N = 40006, T = 37;
-int a[N], b[N], L[N], R[N], pos[N], c[T][T][N], f[T][T][2], now[2];
+const int N = 40006, T = 806;
+int a[N], b[N], c[N], L[N], R[N], pos[N], f[T][T];
+vector<int> e[N];
 
-inline void work(int x, int y, int num) {
-    ++c[x][y][num];
-    if (c[x][y][num] > now[0] || (c[x][y][num] == now[0] && num < now[1])) {
-        now[1] = num;
-        now[0] = c[x][y][num];
+int find(int x, int l, int r) {
+    return upper_bound(e[x].begin(), e[x].end(), r) - lower_bound(e[x].begin(), e[x].end(), l);
+}
+
+void work(int x, int l, int r, int &ans, int &cnt) {
+    int w = find(x, l, r);
+    if (w > cnt || (w == cnt && x < ans)) {
+        cnt = w;
+        ans = x;
     }
 }
 
 int ask(int l, int r) {
     int p = pos[l], q = pos[r];
+    int ans = 0, cnt = 0;
+    if (p == q) {
+        for (int i = l; i <= r; i++) {
+            work(a[i], l, r, ans, cnt);
+
+        }
+        return b[ans];
+    }
     int x = 0, y = 0;
     if (p + 1 <= q - 1) {
-        x = p + 1, y = q - 1;
+        x = p + 1;
+        y = q - 1;
     }
-    memcpy(now, f[x][y], sizeof now);
-    if (p == q) {
-        for (int i = l; i <= r; i++) work(x, y, a[i]);
-        for (int i = l; i <= r; i++) --c[x][y][a[i]];
-    } else {
-        for (int i = l; i <= R[p]; i++) {
-            work(x, y, a[i]);
-        }
-        for (int i = L[q]; i <= r; i++) {
-            work(x, y, a[i]);
-        }
-        for (int i = l; i <= R[p]; i++) {
-            c[x][y][a[i]]--;
-        }
-        for (int i = L[q]; i <= r; i++) {
-            c[x][y][a[i]]--;
-        }
+    for (int i = l; i <= R[p]; i++) {
+        work(a[i], l, r, ans, cnt);
     }
-    return b[now[1]];
+    for (int i = L[q]; i <= r; i++) {
+        work(a[i], l, r, ans, cnt);
+    }
+    if (f[x][y]) {
+        work(f[x][y], l, r, ans, cnt);
+    }
+    return b[ans];
 }
 
 int main() {
@@ -81,12 +86,13 @@ int main() {
     memcpy(b, a, sizeof b);
     sort(b + 1, b + n + 1);
     int tot = unique(b + 1, b + n + 1) - (b + 1);
-    rep(i, 1, n + 1) {
+    for (int i = 1; i <= n; i++) {
         a[i] = lower_bound(b + 1, b + tot + 1, a[i]) - b;
+        e[a[i]].push_back(i);
     }
-    int t = (int)pow((double)n, (double)1 / 3);
+    int t = sqrt(log(n) / log(2) * n);
     int len = t ? n / t : n;
-    rep(i, 1, t + 1) {
+    for (int i = 1; i <= t; i++) {
         L[i] = (i - 1) * len + 1;
         R[i] = i * len;
     }
@@ -99,19 +105,16 @@ int main() {
             pos[j] = i;
         }
     }
-    memset(c, 0, sizeof c);
     memset(f, 0, sizeof f);
     for (int i = 1; i <= t; i++) {
-        for (int j = i; j <= t; j++) {
-            for (int k = L[i]; k <= R[j]; k++) {
-                c[i][j][a[k]]++;
+        memset(c, 0, sizeof c);
+        int cnt = 0, ans = 0;
+        for (int j = L[i]; j <= n; j++) {
+            if (++c[a[j]] > cnt || (c[a[j]] == cnt && a[j] < ans)) {
+                cnt = c[a[j]];
+                ans = a[j];
             }
-            for (int k = 1; k <= tot; k++) {
-                if (c[i][j][k] > f[i][j][0]) {
-                    f[i][j][0] = c[i][j][k];
-                    f[i][j][1] = k;
-                }
-            }
+            f[i][pos[j]] = ans;
         }
     }
     int x = 0;
@@ -120,7 +123,9 @@ int main() {
         cin >> l >> r;
         l = (l + x - 1) % n + 1;
         r = (r + x - 1) % n + 1;
-        if (l > r) swap(l, r);
+        if (l > r) {
+            swap(l, r);
+        }
         x = ask(l, r);
         cout << x << "\n";
     }
