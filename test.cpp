c8f0383c3c3a120f376b78c9e192f963daa81083
huqinghao <huqinghao@outlook.com>
2024-11-06 07:50:02 -0500

    up

--- test.cpp
+++ test.cpp
@@ -1,11 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, a, n) for (int i=a; i<n; i++)
-#define per(i, a, n) for (int i=a; i>=n; i--)
 typedef long long ll;
 typedef double db;
-typedef vector<int> VI;
-typedef pair<int, int> PII;
 const db eps = 1e-9;
 const db PI = acos(-1.0);
 const int INF = 0x3f3f3f3f;
@@ -14,16 +10,20 @@ ll gcd(ll a, ll b) {return !b ? a : gcd(b, a%b);}
 ll powmod(ll a, ll b, ll p) {ll res=1;for(;b;b>>=1){if(b&1){res=res*a%p;}a=a*a%p;}return res;}
 ll lcm(ll a, ll b) {return a /gcd(a,b)*b;}
 
-void timeUse() {
-	cout << (db)clock() / CLOCKS_PER_SEC << " s\n";
-}
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-	srand(time(0));
-	cout << rand() << '\n';
-    
-    timeUse();
+	vector<int> a(9, 0);
+	iota(a.begin(), a.end(), 1);
+	do {
+		int x = a[0] * 100 + a[1] * 10 + a[2];
+		int y = a[3] * 100 + a[4] * 10 + a[5];
+		int z = a[6] * 100 + a[7] * 10 + a[8];
+		if (2 * x == y && 3 * x == z) {
+			cout << x << " " << y << " " << z << '\n';
+		}
+	} 
+	while (next_permutation(a.begin(), a.end()));
+	
     return 0;
 }
